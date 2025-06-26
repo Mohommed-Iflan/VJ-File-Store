dee45eeefcb92b70799270877144d09067deadb1
Mohommed-Iflan <mohommediflaan@gmail.com>
2025-06-26 07:37:43 +0400

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "25399581"))
 API_HASH = environ.get("API_HASH", "78e8a7d45e41484937c47acfdf1f6433")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7774424472:AAGMvtqkd9XVPZOX9u6bO-2AxDaOy14hcSI")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7898302379:AAH2w4kbPIukPTmTKOzCnUJ7n8jRWAjRUrI")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6322672707').split()]
